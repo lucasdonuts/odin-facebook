754427315be83bafacefe9c3a5b2770efe4d3513
Lucas <lucasdonuts@gmail.com>
2021-03-11 16:01:38 -0600

    Profile pictures now show on user's profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def avatar(user, size=32)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "x#{size}", crop: "#{size}x#{size}+#{size/2}+0")
+    else
+      'default_avatar.jpg'
+    end
+  end
 end
